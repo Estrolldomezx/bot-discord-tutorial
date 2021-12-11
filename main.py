18d4e954bf706cee58f9bdf534b41a311a9462b8
Wanish Sengkhim <wanish45037.2@gmail.com>
2021-12-11 14:50:53 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,246 @@
+import discord
+import asyncio
+import random
+import os #add
+
+#import module
+from discord.ext import commands
+from datetime import datetime, timedelta
+from discord.utils import get #edit
+from discord import FFmpegPCMAudio
+from youtube_dl import YoutubeDL
+from song import songAPI
+
+from dotenv import load_dotenv #add
+load_dotenv()
+token = os.getenv('TOKEN')
+
+message_lastseen = datetime.now()
+message2_lastseen = datetime.now()
+
+bot = commands.Bot(command_prefix='+', help_command=None)
+
+
+aboutSong = songAPI()
+
+#bot online
+@bot.event 
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def test(ctx, *, par):
+    await ctx.channel.send("You typed {0}".format(par))
+
+@bot.command()
+async def help(ctx):
+    emBed = discord.Embed(title="Tutorial bot help", description =" All available bot command", color=0x42f5a7)
+    emBed.add_field(name="+help", value = "Get help command", inline=True)
+    emBed.add_field(name="+test", value = "Respond message that you've send", inline=True)
+    emBed.add_field(name="+bot", value = "Introduce bot", inline=True)
+    emBed.set_thumbnail(url='https://www.pngall.com/wp-content/uploads/2016/06/Adidas-Logo-PNG-Clipart.png')
+    emBed.set_image(url='https://upload.wikimedia.org/wikipedia/commons/0/00/Liverpool_vs._Chelsea%2C_UEFA_Super_Cup_2019-08-14_53.jpg')
+    emBed.set_author(name="Dome", icon_url="https://www.freeiconspng.com/thumbs/human-icon-png/human-icon-png-png-20.png")
+    emBed.set_footer(text ="footer", icon_url='https://www.pngall.com/wp-content/uploads/2016/06/Adidas-Logo-PNG-Clipart.png')
+
+    await ctx.channel.send(embed=emBed)
+
+@bot.command()
+async def schedule(ctx):
+    emBed = discord.Embed(title="Class schedule", color=0xf0ffff)
+    emBed.set_image(url='https://cdn-icons-png.flaticon.com/512/470/470326.png')
+    await ctx.channel.send(embed=emBed)
+
+#reminder
+@bot.command()
+async def remind(ctx, time, *, task):
+    #+remind 1m Play games
+    def convert(time):
+        pos = ['s', 'm', 'h', 'd']
+        #set time to list
+        time_dict = {"s": 1, "m": 60, "h": 3600, "d": 3600*24}
+        #match unit and sec
+        unit = time[-1]
+
+        if unit not in pos:
+            return -1
+        try:
+            val = int(time[:-1])
+        except:
+            return -2
+        
+        return val * time_dict[unit]
+    
+    convert_time = convert(time)
+    times = convert_time
+    half_time = convert_time/2
+    quater_time = half_time/2
+
+    #quater
+    qday = quater_time // (24 * 3600)
+ 
+    quater_time = quater_time % (24 * 3600)
+    qhour = quater_time // 3600
+ 
+    quater_time %= 3600
+    qminutes = quater_time // 60
+ 
+    quater_time %= 60
+    qseconds = quater_time
+
+    #half
+    hday = half_time // (24 * 3600)
+ 
+    half_time = half_time % (24 * 3600)
+    hhour = half_time // 3600
+ 
+    half_time %= 3600
+    hminutes = half_time // 60
+ 
+    half_time %= 60
+    hseconds = half_time
+
+    #full
+    day = convert_time // (24 * 3600)
+
+    convert_time = convert_time % (24 * 3600)
+    hour = convert_time // 3600
+ 
+    convert_time %= 3600
+    minutes = convert_time // 60
+ 
+    convert_time %= 60
+    seconds = convert_time
+
+    # return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+
+    if convert_time == -1:
+        await ctx.send("You didn't answer time correctly")
+        return
+
+    if convert_time == -2:
+        await ctx.send("The time must be an integer")
+        return
+
+    #reminder start
+    emBed = discord.Embed(title="Lung Pol's Reminder Start", color=0x42f5a7)
+    emBed.add_field(name="Reminder", value = task, inline=False)
+    emBed.add_field(name="From", value = ctx.author.mention, inline=False)
+    emBed.add_field(name="Time Left", value = f"{day} days {hour} hours {minutes} minutes {seconds} seconds", inline=False)  
+    msg = await ctx.channel.send(embed=emBed)
+    await msg.add_reaction('✅')
+
+    #reminder idle
+    await asyncio.sleep(times/2)
+    emBed = discord.Embed(title="Lung Pol's Reminder", color=0x42f5a7)
+    emBed.add_field(name="Reminder", value = task, inline=False)
+    emBed.add_field(name="From", value = ctx.author.mention, inline=False)
+    if hday ==0.0:
+        hday = int(hday)
+    if hhour ==0.0:
+        hhour = int(hhour)
+    if hminutes ==0.0:
+        hminutes = int(hminutes)
+    if hseconds ==0.0:
+        hseconds = int(hseconds)
+    emBed.add_field(name="Time Left", value = f"{hday} days {hhour} hours {hminutes} minutes {hseconds} seconds", inline=False)  
+    msg = await ctx.channel.send(embed=emBed)
+    await msg.add_reaction('✅')
+    
+    await asyncio.sleep(times/4)
+    emBed = discord.Embed(title="Lung Pol's Reminder", color=0x42f5a7)
+    emBed.add_field(name="Reminder", value = task, inline=False)
+    emBed.add_field(name="From", value = ctx.author.mention, inline=False)
+    if qday ==0.0:
+        qday = int(qday)
+    if qhour ==0.0:
+        qhour = int(qhour)
+    if qminutes ==0.0:
+        qminutes = int(qminutes)
+    if qseconds ==0.0:
+        qseconds = int(qseconds)
+    emBed.add_field(name="Time Left", value = f"{qday} days {qhour} hours {qminutes} minutes {qseconds} seconds", inline=False)  
+    msg = await ctx.channel.send(embed=emBed)
+    await msg.add_reaction('✅')
+    
+    #reminder finish
+    await asyncio.sleep(times/4)
+    emBed = discord.Embed(title="Lung Pol's Reminder Finish !", color=0x42f5a7)
+    emBed.add_field(name="Reminder", value = task, inline=False)
+    emBed.add_field(name="From", value = ctx.author.mention, inline=False)
+    emBed.add_field(name="Time Left", value = f"0 days 0 hours 0 minutes 0 seconds", inline=False)  
+    msg = await ctx.channel.send(embed=emBed)
+    await msg.add_reaction('✅')
+
+# polling
+@bot.command()
+async def poll(ctx, *, message):
+    emBed = discord.Embed(title="Poll of the day", description=f"{message}", color=0x42f5a7)
+    msg = await ctx.channel.send(embed=emBed)
+    await msg.add_reaction('👍')
+    await msg.add_reaction('👎')
+
+#bot interact message
+@bot.event
+async def on_message(message):
+    global message_lastseen, message2_lastseen
+    if message.content == '!Hello':
+        print(message.channel)
+        await message.channel.send('Hello ' + str(message.author.name))
+    
+    elif message.content == '!bye':
+        print(message.channel)
+        await message.channel.send('Good bye')
+
+    #introduce bot
+    elif message.content == '!bot' and datetime.now() >= message_lastseen:
+        message_lastseen = datetime.now() + timedelta(seconds=5)
+        print(message.channel)
+        await message.channel.send('I am '+ str(bot.user.name))
+        print('{0} calling user when {1} and you can use {2}'. format(message.author.name, datetime.now(), message_lastseen))
+
+    #introduce user
+    elif message.content == '!user' and datetime.now() >= message2_lastseen:
+        message2_lastseen = datetime.now() + timedelta(seconds=5)
+        await message.channel.send('You are '+ str(message.author.name))
+
+    elif message.content == '!logout':
+        await bot.logout()
+
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def play(ctx, *, search: str):
+    await aboutSong.play(ctx, search)
+
+@bot.command()
+async def stop(ctx):
+    await aboutSong.stop(ctx)
+
+@bot.command()
+async def pause(ctx):
+    await aboutSong.pause(ctx)
+
+@bot.command()
+async def resume(ctx):
+    await aboutSong.resume(ctx)
+
+@bot.command()
+async def leave(ctx):
+    await aboutSong.leave(ctx) #edit
+
+@bot.command()
+async def skip(ctx):
+    await aboutSong.skip(ctx) #edit
+
+@bot.command()
+async def queueList(ctx):
+    await aboutSong.queueList(ctx) #edit
+
+
+bot.run(token)   
+
+
+
---
+++ song.py
@@ -0,0 +1,260 @@
+import discord
+from discord.utils import get
+import youtube_dl
+import asyncio
+from async_timeout import timeout
+from functools import partial
+import itertools
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn',
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5" ## song will end if no this line
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+
+    def __init__(self, source, *, data, requester):
+        super().__init__(source)
+        self.requester = requester
+
+        self.title = data.get('title')
+        self.web_url = data.get('webpage_url')
+
+        # YTDL info dicts (data) have other useful information you might want
+        # https://github.com/rg3/youtube-dl/blob/master/README.md
+
+    def __getitem__(self, item: str):
+        """Allows us to access attributes similar to a dict.
+        This is only useful when you are NOT downloading.
+        """
+        return self.__getattribute__(item)
+
+    @classmethod
+    async def create_source(cls, ctx, search: str, *, loop, download=False):
+        loop = loop or asyncio.get_event_loop()
+
+        to_run = partial(ytdl.extract_info, url=search, download=download)
+        data = await loop.run_in_executor(None, to_run)
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        await ctx.send(f'```ini\n[Added {data["title"]} to the Queue.]\n```') #delete after can be added
+
+        if download:
+            source = ytdl.prepare_filename(data)
+        else:
+            return {'webpage_url': data['webpage_url'], 'requester': ctx.author, 'title': data['title']}
+
+        return cls(discord.FFmpegPCMAudio(source, **ffmpeg_options), data=data, requester=ctx.author)
+
+    @classmethod
+    async def regather_stream(cls, data, *, loop):
+        """Used for preparing a stream, instead of downloading.
+        Since Youtube Streaming links expire."""
+        loop = loop or asyncio.get_event_loop()
+        requester = data['requester']
+
+        to_run = partial(ytdl.extract_info, url=data['webpage_url'], download=False)
+        data = await loop.run_in_executor(None, to_run)
+
+        return cls(discord.FFmpegPCMAudio(data['url'], **ffmpeg_options), data=data, requester=requester)
+
+class MusicPlayer:
+    """A class which is assigned to each guild using the bot for Music.
+    This class implements a queue and loop, which allows for different guilds to listen to different playlists
+    simultaneously.
+    When the bot disconnects from the Voice it's instance will be destroyed.
+    """
+
+    __slots__ = ('bot', '_guild', '_channel', '_cog', 'queue', 'next', 'current', 'np', 'volume')
+
+    def __init__(self, ctx):
+        self.bot = ctx.bot
+        self._guild = ctx.guild
+        self._channel = ctx.channel
+        self._cog = ctx.cog
+
+        self.queue = asyncio.Queue()
+        self.next = asyncio.Event()
+
+        self.np = None  # Now playing message
+        self.volume = .5
+        self.current = None
+
+        ctx.bot.loop.create_task(self.player_loop())
+
+    async def player_loop(self):
+        """Our main player loop."""
+        await self.bot.wait_until_ready()
+
+        while not self.bot.is_closed():
+            self.next.clear()
+
+            try:
+                # Wait for the next song. If we timeout cancel the player and disconnect...
+                async with timeout(300):  # 5 minutes...
+                    source = await self.queue.get()
+            except asyncio.TimeoutError:
+                del players[self._guild]
+                return await self.destroy(self._guild)
+
+            if not isinstance(source, YTDLSource):
+                # Source was probably a stream (not downloaded)
+                # So we should regather to prevent stream expiration
+                try:
+                    source = await YTDLSource.regather_stream(source, loop=self.bot.loop)
+                except Exception as e:
+                    await self._channel.send(f'There was an error processing your song.\n'
+                                             f'```css\n[{e}]\n```')
+                    continue
+
+            source.volume = self.volume
+            self.current = source
+
+            self._guild.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
+            self.np = await self._channel.send(f'**Now Playing:** `{source.title}` requested by '
+                                               f'`{source.requester}`')
+            await self.next.wait()
+
+            # Make sure the FFmpeg process is cleaned up.
+            source.cleanup()
+            self.current = None
+
+            try:
+                # We are no longer playing this song...
+                await self.np.delete()
+            except discord.HTTPException:
+                pass
+
+    async def destroy(self, guild):
+        """Disconnect and cleanup the player."""
+        await self._guild.voice_client.disconnect()
+        return self.bot.loop.create_task(self._cog.cleanup(guild))
+
+
+class songAPI:
+    def __init__(self):
+        self.players = {}
+        
+    async def play(self, ctx,search: str):
+        self.bot = ctx.bot
+        self._guild = ctx.guild
+        channel = ctx.author.voice.channel
+        voice_client = get(self.bot.voice_clients, guild=ctx.guild)
+        
+        if voice_client == None:
+            await ctx.channel.send("Joined")
+            await channel.connect()
+            voice_client = get(self.bot.voice_clients, guild=ctx.guild)
+
+        await ctx.trigger_typing()
+
+        _player = self.get_player(ctx)
+        source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop, download=False)
+
+        await _player.queue.put(source)
+
+    players = {}
+    def get_player(self, ctx):
+        try:
+            player = self.players[ctx.guild.id]
+        except:
+            player = MusicPlayer(ctx)
+            self.players[ctx.guild.id] = player
+        
+        return player
+
+
+    async def stop(self, ctx):
+        voice_client = get(self.bot.voice_clients, guild=ctx.guild)
+        if voice_client == None:
+            await ctx.channel.send("bot stop playing")
+            return
+        
+        #case bot live in different voice channel
+        if voice_client.channel != ctx.author.voice.channel:
+            await ctx.channel.send("bot is currently to {0}" .format(voice_client.channel))
+            return
+        
+        voice_client.stop()
+
+    async def pause(self, ctx):
+        voice_client = get(self.bot.voice_clients, guild=ctx.guild)
+        if voice_client == None:
+            await ctx.channel.send("bot pause playing")
+            return
+        
+        #case bot live in different voice channel
+        if voice_client.channel != ctx.author.voice.channel:
+            await ctx.channel.send("bot is currently to {0}" .format(voice_client.channel))
+            return
+        
+        voice_client.pause()
+
+    async def resume(self, ctx):
+        voice_client = get(self.bot.voice_clients, guild=ctx.guild)
+        if voice_client == None:
+            await ctx.channel.send("bot resume playing")
+            return
+        
+        #case bot live in different voice channel
+        if voice_client.channel != ctx.author.voice.channel:
+            await ctx.channel.send("bot is currently to {0}" .format(voice_client.channel))
+            return
+        
+        voice_client.resume()
+
+    async def leave(self, ctx):
+        del self.players[ctx.guild.id]
+        await ctx.voice_client.disconnect()
+
+    async def skip(self, ctx):
+        voice_client = get(self.bot.voice_clients, guild  = ctx.guild)
+
+        if voice_client == None or not voice_client.is_connected():
+            await ctx.channel.send("Bot is not connect to voice channel", delete_after = 10)
+            return
+
+        if voice_client.is_paused():
+            pass
+
+        elif not voice_client.is_playing():
+            return
+        voice_client.stop()
+        await ctx.send(f"{ctx.author} : skipped the song!")
+
+    async def queueList(self, ctx):
+        voice_client = get(self.bot.voice_clients, guild  = ctx.guild)
+
+        if voice_client == None or not voice_client.is_connected():
+            await ctx.channel.send("Bot is not connect to voice channel", delete_after = 10)
+            return
+        
+        player = self.get_player(ctx)
+        if player.queue.empty():
+            return await ctx.send('There are currently no more queued songs')
+        
+        upcoming = list(itertools.islice(player.queue._queue, 0, player.queue.qsize()))
+        fmt = '\n'.join(f'{_["title"]}' for _ in upcoming)
+        embed = discord.Embed(title = f'Upcoming - Next {len(upcoming)}', description = fmt)
+        await ctx.send(embed = embed)
